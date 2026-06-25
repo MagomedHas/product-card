8840c4267359eca329c1c1a3befc76f2fe9e63cc
MagomedHas <101834124+MagomedHas@users.noreply.github.com>
2026-06-25 17:25:41 +0300

    buttonInversion.classList.toggle

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
-const cardFirst = document.querySelector('.product-card');
-const cardAll = document.querySelectorAll('.product-card');
+const firstCard = document.querySelector('.product-card');
+const allCards = document.querySelectorAll('.product-card');
 const greenColorHash = '#00ff00';
 const blueColorHash = '#00f';
 const heading = document.querySelector('.heading');
-const buttonInversion = document.getElementById('button-inversion');
+const invertButton = document.getElementById('button-inversion');
 const buttonRecolorProduct = document.getElementById('button-recolor-product');
 const buttonRecolorProductAll = document.getElementById(
   'button-recolor-product-all'
 );
-const buttonGoogle = document.getElementById('button-google');
+const openGoogleBtn = document.getElementById('button-google');
 
 buttonRecolorProduct.addEventListener('click', () => {
-  cardFirst.style.backgroundColor = greenColorHash;
+  firstCard.style.backgroundColor = greenColorHash;
 });
-buttonInversion.addEventListener('click', () => {
-  switchCssClass(buttonInversion, 'button-inversion');
+
+invertButton.addEventListener('click', () => {
+  invertButton.classList.toggle('button-inversion');
 });
+
 buttonRecolorProductAll.addEventListener('click', () => {
-  cardAll.forEach((card) => {
+  allCards.forEach((card) => {
     card.style.backgroundColor = blueColorHash;
   });
 });
 
-buttonGoogle.addEventListener('click', openGoogle);
+openGoogleBtn.addEventListener('click', openGoogle);
 heading.addEventListener('mouseover', () => {
   console.log(heading.textContent);
 });
@@ -30,13 +32,5 @@ function openGoogle() {
   const answer = confirm('Вы действительно хотите открыть Google?');
   if (answer) {
     window.open('https://google.com');
-  }
-}
-
-function switchCssClass(element, nameClass) {
-  if (element.classList.contains(nameClass)) {
-    element.classList.remove(nameClass);
-  } else {
-    element.classList.add(nameClass);
   }
 }
