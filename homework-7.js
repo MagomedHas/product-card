e180f80418609e0ca6d05ef64200ce06a7cd04be
MagomedHas <101834124+MagomedHas@users.noreply.github.com>
2026-06-29 12:07:22 +0300

    function cityTemptratureOutputInCelsius

---
+++ homework-7.js
@@ -0,0 +1,9 @@
+function cityTemptratureOutputInCelsius(city, temptratureInCelsius) {
+  console.log(
+    'Сейчас в ' +
+      city +
+      ' температура — ' +
+      temptratureInCelsius +
+      ' градусов по Цельсию'
+  );
+}
