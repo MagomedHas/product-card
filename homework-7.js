7de865fa4cd1c97fb60a7fb59120677a07690d06
MagomedHas <101834124+MagomedHas@users.noreply.github.com>
2026-06-29 12:29:53 +0300

    function checkSpeedLight

--- homework-7.js
+++ homework-7.js
@@ -7,3 +7,14 @@ function cityTemptratureOutputInCelsius(city, temptratureInCelsius) {
       ' градусов по Цельсию'
   );
 }
+
+function checkSpeedLight(speed) {
+  const speedlight = 299_792_458;
+  if (speed > speedlight) {
+    console.log('Сверхсветовая скорость');
+  } else if (speed < speedlight) {
+    console.log('Субсветовая скорость');
+  } else {
+    console.log('Скорость света');
+  }
+}
