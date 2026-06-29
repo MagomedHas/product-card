8df99b70574b9b72378bdd9550178b8bfa9981ad
MagomedHas <101834124+MagomedHas@users.noreply.github.com>
2026-06-29 16:22:18 +0300

    function logProductPurchase

--- homework-7.js
+++ homework-7.js
@@ -2,7 +2,7 @@ function logCityTempC(city, tempC) {
   console.log(`Сейчас в ${city} температура — ${tempC} градусов по Цельсию`);
 }
 
-function checkSpeedToLight(speed) {
+function logCheckSpeedToLight(speed) {
   const SPEED_OF_LIGHT = 299_792_458;
   if (speed > SPEED_OF_LIGHT) {
     console.log('Сверхсветовая скорость');
@@ -10,5 +10,17 @@ function checkSpeedToLight(speed) {
     console.log('Субсветовая скорость');
   } else {
     console.log('Скорость света');
+  }
+}
+
+const productName = 'Mars';
+const productPrice = 299_792_458; // ;)
+
+function logProductPurchase(budget) {
+  if (budget >= productPrice) {
+    console.log(`${productName} приобретён. Спасибо за покупку!`);
+  } else {
+    const shortage = productPrice - budget;
+    console.log(`Вам не хватает ${shortage}$, пополните баланс`);
   }
 }
