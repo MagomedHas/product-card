5160c23fcc63df6360bccf0b480db1bf6483032d
MagomedHas <101834124+MagomedHas@users.noreply.github.com>
2026-07-06 19:14:49 +0300

    const client

---
+++ homework-8.js
@@ -0,0 +1,12 @@
+const client = {
+  firstName: 'Али',
+  lastName: 'Хабибов',
+  gender: 'М',
+  age: 43,
+  country: 'Россия',
+  city: 'Сочи',
+  email: 'alix43@mail.ru',
+  job: 'Строитель',
+  position: 'Прораб',
+  relationshipStatus: 'Женат',
+};
