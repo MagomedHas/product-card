8effd42c99a64deebb7af454aa5153bd0acfae36
MagomedHas <101834124+MagomedHas@users.noreply.github.com>
2026-07-06 20:09:39 +0300

    function addMaxSpeed

--- homework-8.js
+++ homework-8.js
@@ -14,9 +14,15 @@ const client = {
 const car = {
   make: 'Toyota',
   model: 'Camry',
-  yearOfManufacture: '2020',
+  yearOfManufacture: 2020,
   color: 'серебристый',
   typeOfTransmission: 'автоматическая',
 };
 
 car.owner = client;
+
+function addMaxSpeed(objCar) {
+  if (!objCar.maxSpeed) {
+    objCar.maxSpeed = 250;
+  }
+}
